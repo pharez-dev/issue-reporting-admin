03b53ab3f402c6c28eb1ad3896646ee215921490
pharez <pharezwk@gmail.com>
2020-08-27 12:13:35 +0300

    heroku

--- .history/constants/Globals_20200827121243.js
+++ .history/constants/Globals_20200827121243.js
@@ -3,8 +3,7 @@ const globals = {
   BASE_URL:
     // "https://8081-d4adba9e-5c36-4db6-8052-c0e777ae2657.ws-eu01.gitpod.io"
     // BASE_URL: "http://192.168.100.4:8081"\
-    //   "http://192.168.0.173:8080",
-    "https://issue-reporting.herokuapp.com",
+ //   "http://192.168.0.173:8080",
 };
 
 export default globals;
